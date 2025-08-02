f77712f60b5cd154da3c5ed9af0f3d62a0cf5ef8
santiagotf <168837358+SATF27@users.noreply.github.com>
2025-08-01 21:22:51 -0500

    Add base class for queues

---
+++ queues/queue.py
@@ -0,0 +1,26 @@
+from .node import Node
+
+class Queue():
+    def __init__(self):
+        self._front = None
+    
+    def enqueue(self, data):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def __len__(self):
+        pass
+    
+    def __iter__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __add__(self):
+        pass
+
+    def is_empty(self):
+        return self._front is None
